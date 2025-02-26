c2d4caefb5a1df2ffad600bdfb7b8d443e266e66
frahay123 <frankydude04@gmail.com>
2025-02-26 14:52:31 -0500

    update

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-6 text-center text-sm font-mono text-lighter/60">
-      <p>Built with Next.js & Tailwind CSS</p>
-      <p>© {new Date().getFullYear()} Frank L. All Rights Reserved.</p>
-    </footer>
-  );
-}
