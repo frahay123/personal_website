266d663a75a2d6e021747e5094c26bc470cdeb04
frahay123 <frankydude04@gmail.com>
2025-02-26 15:06:02 -0500

    update
    added research discription

--- src/pages/research.js
+++ src/pages/research.js
@@ -1,22 +1,13 @@
-const projects= [
-    { name: "ViPhy", description: "The MGE-TF project analyzes the possible regulation of phage genes by host transcription factors (TF), integrating information on the score extremeness, colocalization and intergenicity of predicted TF-binding sites in the phage genome."}
-  ];
-  
+import styles from "../styles/research.module.css"; 
   export default function Projects() {
     return (
-      <div className="min-h-screen bg-gray-900 text-white p-6">
-        <h1 className="text-4xl font-bold mb-6">Projects hfoweihfoweuhofuewhofuhwoeufh</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {projects.map((project, index) => (
-            <div key={index} className="p-6 bg-gray-800 rounded-lg">
-              <h2 className="text-2xl font-bold">{project.name}</h2>
-              <p className="text-gray-400 mt-2 size-10">{project.description}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+  <div className={styles.container}>
+    <h1 className={styles.title}>Research</h1>
+    <h1 className={styles.subtitle}> UMBC Erill Lab: ViPhy</h1>
+    <h1 className={styles.description}> The MGE-TF project analyzes the possible regulation of phage genes by host transcription factors (TF), integrating information on the score extremeness, colocalization and intergenicity of predicted TF-binding sites in the phage genome.
+    In the project, my assigment was to make the program more efficient and run faster. I accomplished this by using avoiding double computations of the genomes. I also changed the way random generation worked for bootstrap to generate all the numbers at once using numpy libraries instead of using the built in python random number generator. 
+    This resulted in the script running 70% percent faster.  </h1>
+  </div>
     );
   }
-
- 
 
